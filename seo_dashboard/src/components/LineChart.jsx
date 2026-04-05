028bd4df364bb76c405320a62213da09ec88567c
frez <rezende.f@outlook.com>
2026-04-05 18:13:59 +0800

    RankigChart being updated to set earlliest date for plotting lines without jumping back to dates... not finished

--- seo_dashboard/src/components/LineChart.jsx
+++ seo_dashboard/src/components/LineChart.jsx
@@ -6,12 +6,24 @@ function RankingChart() {
     
     const ranked = graphData.ranked;
 
-    // Get unique keywords
-    // ranked.map pulls all keywords
-    // Set removes duplicates
-    // ... spread operator converts Set obj back into Array
+    /*
+    Get unique keywords
+    ranked.map pulls all keywords
+    Set removes duplicates
+    ... spread operator converts Set obj back into Array
     const keywords = [...new Set(ranked.map(item => item.keyword))];
+    */
 
+    /*
+    Get earliest date, call Date method to create js data object
+    ... spread operator to unpack the array into individual arguments
+    call Math.min() to find the smallest value
+    NOTE: Date objects when used in Math automatically converts to number 
+    milliseconds since 1970 
+    */
+    const earliestDate = new Date(Math.min
+        (...ranked.map(item => new Date(item.date)))
+    );
     // Build a trace for each keyword
     const traces = keywords.map(keyword => {  // Loop through unique keywords
         // Get entries that belong to current keywords from ranked
@@ -32,7 +44,11 @@ function RankingChart() {
             layout={{
                 title: 'Keyword Rankings Over Time',
                 yaxis: { autorange: 'reversed' }, // position 1 at top
-                xaxis: { title: 'Date' },
+                xaxis: {
+                    title: 'Date',
+                    range: [earliestDate]
+                },
+
             }}
         />
     )
